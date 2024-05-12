393fe92840d11cac7012656c16eeb309b7147b14
Cciradih <trash@cciradih.eu.org>
2024-05-12 18:10:16 +0800

    image send

---
+++ custom_nodes/image_send.py
@@ -0,0 +1,47 @@
+from PIL import Image
+from io import BytesIO
+import numpy
+import requests
+
+class SendImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "url": ("STRING", {}),
+                "key": ("STRING", {}),
+                "seed": ("INT", {}),
+                "callback": ("STRING", {}),
+            },
+        }
+ 
+    RETURN_TYPES = ()
+ 
+    FUNCTION = "send_images"
+ 
+    OUTPUT_NODE = True
+ 
+    CATEGORY = "api/image"
+
+    def send_images(self, images, url, key, seed, callback):
+        names = []
+        for i, image in enumerate(images):
+            bytes = BytesIO()
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(numpy.clip(i, 0, 255).astype(numpy.uint8))
+            img.save(bytes, format="PNG")
+            name = str(seed) + ".png"
+            path  = "/" + name
+            requests.post(url+path, headers={"key": key}, data=bytes.getvalue())
+            names.append(name)
+        requests.post(callback, json=names)
+        return {}
+
+NODE_CLASS_MAPPINGS = {
+    "SendImage": SendImage
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SendImage": "Send Image"
+}
